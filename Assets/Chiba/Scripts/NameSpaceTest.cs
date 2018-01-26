eabf87c95fd8d75ab19735b28a1d0dd0c44dbd06
Chiba_Mizuki <bigwave_whale@yahoo.co.jp>
2018-01-27 03:32:51 +0900

    米山シングルトン講座プッシュ

---
+++ Assets/Chiba/Scripts/NameSpaceTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Chiba;
+
+public class NameSpaceTest : MonoBehaviour {
+	bool isMuteki = false;
+
+	IEnumerator TestCoroutine(){
+		yield return new WaitForSeconds (3f);
+		isMuteki = false;
+	} 
+
+	void Damage(){
+		if (isMuteki) {
+			return;
+		}
+		Test.Instance.HP -= 10;
+		isMuteki = true;
+		StartCoroutine (TestCoroutine());
+	}
+}
---
+++ Assets/Chiba/Scripts/Test.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chiba{
+	public class Test : MonoSingleton<Test> {
+		public int HP = 100;
+	}
+}
