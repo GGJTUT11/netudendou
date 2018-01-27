1336e85eb78bf7e620a0be703c94a50764121e5e
pc\Matsuda Rio <rio.rio9259@gmail.com>
2018-01-27 20:11:57 +0900

    α版

---
+++ Assets/Matsuda/Rock.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Rock : MonoBehaviour
+{
+    private float speed = -5.0f;
+    private Vector3 moveDirection = Vector3.zero;
+    public static bool isClear;
+    Rigidbody rb;
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+    void Update()
+    {
+        if (GameObject.Find("RockSwitch") == true)
+        {
+            rb.position += new Vector3(1, 0, 0) * speed * Time.deltaTime;
+        }
+    }
+    public void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "ningen")
+        {
+            isClear = false;
+            SceneManager.LoadScene("EndScene");
+        }
+    }
+    public static bool GetIsClear()
+    {
+        return isClear;
+    }
+}
