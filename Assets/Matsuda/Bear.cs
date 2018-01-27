0abfb7d933a2c8595c182874f12edb9ca138dd37
pc\Matsuda Rio <rio.rio9259@gmail.com>
2018-01-27 13:20:45 +0900

    熊追加

---
+++ Assets/Matsuda/Bear.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bear : MonoBehaviour
+{
+    private float speed = 1.0f;
+    private Vector3 moveDirection = Vector3.zero;
+    private float x = -2.0f;
+    int layer_Player;
+    int layer_Bear;
+    int layer_ningen;
+    bool collisionflag = true;
+    void Start()
+    {
+        layer_Player = LayerMask.NameToLayer("Player");
+        layer_Bear = LayerMask.NameToLayer("bear");
+        layer_ningen = LayerMask.NameToLayer("ningen");
+    }
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+        moveDirection = new Vector3(x, 0, 0);
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= speed;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "ningen" && collisionflag == true)
+        {
+            x = 0;
+            Debug.Log("GameOver");
+        }
+        else if (other.transform.tag == "Player" && GameObject.Find("Player").GetComponent<PlayerMove>().Netudendou_Property == 0.0f)
+        {
+            x = 0;
+            collisionflag = false;
+            Physics.IgnoreLayerCollision(layer_Player, layer_Bear);
+            Physics.IgnoreLayerCollision(layer_ningen, layer_Bear);
+        }
+    }
+}
