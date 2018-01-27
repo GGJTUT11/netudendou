ae6b2015592fe2985d0831a99a0e418fb5d63170
pc\Matsuda Rio <rio.rio9259@gmail.com>
2018-01-28 03:58:00 +0900

    最新版

---
+++ Assets/Matsuda/DeleteIceGroud.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteIceGroud : MonoBehaviour {
+    private GameObject icegroundhole;
+	// Use this for initialization
+	void Start () {
+        icegroundhole = GameObject.Find("IceGroundOnHole");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && GameObject.Find("Player").GetComponent<PlayerMove>().Netudendou_Property == 1.0f)
+        {
+            Destroy(icegroundhole.gameObject);
+        }
+    }
+}
---
+++ Assets/Matsuda/IceWall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceWall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player" && GameObject.Find("Player").GetComponent<PlayerMove>().Netudendou_Property == 1.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
