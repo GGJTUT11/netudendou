5a4e8b32865c2b2347f20851f798e0d080dab5ca
Yoneyama Akihiro <m0115360b6@edu.teu.ac.jp>
2018-01-27 09:36:59 +0900

    [demoVol2] 人間の移動速度の変更ができていません

---
+++ Assets/Yoneyama/Script/IcsSwitch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IcsSwitch : MonoBehaviour {
+
+    [SerializeField] GameObject Ice_Ground;
+
+    public void Create_IceGround()
+    {
+        Ice_Ground.SetActive(true);
+    }
+
+    public void Destory_IceGround()
+    {
+        Ice_Ground.SetActive(false);
+    }
+
+}
--- Assets/Yoneyama/Script/PlayerMove.cs
+++ Assets/Yoneyama/Script/PlayerMove.cs
@@ -8,7 +8,9 @@ public class PlayerMove : MonoBehaviour {
     [SerializeField] private float jump = 5f;
     [SerializeField] private Rigidbody rb;
 
-    [SerializeField] private float netudendou_syoki = 0.5f;
+    private bool isground = true;
+
+    private float netudendou_syoki = 0.5f;
 
     [SerializeField] private float netudendou = 0.5f;
     private bool syozi = false;
@@ -18,7 +20,15 @@ public class PlayerMove : MonoBehaviour {
     [SerializeField] private GameObject netudendou_50_Obj;
     [SerializeField] private GameObject netudendou_100_Obj;
 
+    [SerializeField] private float ningen_idou_osoi = 0;
+    [SerializeField] private float ningen_idou_nomale = 1;
+    [SerializeField] private float ningen_idou_hayai = 2;
 
+
+    public void ningen_idouryou(float netudendou)
+    {
+       
+    }
 
     IEnumerator waitrespon()
     {
@@ -38,7 +48,7 @@ public class PlayerMove : MonoBehaviour {
     }
 
     /// <summary>
-    /// プレイヤーの移動
+    /// プレイヤーの移動,ジャンプ
     /// </summary>
     void move()
     {
@@ -47,17 +57,22 @@ public class PlayerMove : MonoBehaviour {
             rb.velocity = new Vector3(Input.GetAxis("Horizontal") * speed, rb.velocity.y, 0f);
         }
 
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W) && isground == true)
         {
             rb.velocity = new Vector3(0f, jump, 0f);
+            isground = false;
         }
     }
 
+    /// <summary>
+    /// Objのイジェクト
+    /// </summary>
     void Objhakidasi()
     {
         if (Input.GetKeyDown(KeyCode.Space) && syozi == true && netudendou == 0.0f && respon_OK == true)
         {
             Instantiate(netudendou_0_Obj, transform.position, transform.rotation);
+            netudendou = netudendou_syoki;
             syozi = false;
             respon_OK = false;
         }
@@ -65,6 +80,7 @@ public class PlayerMove : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.Space) && syozi == true && netudendou == 0.5f && respon_OK == true)
         {
             Instantiate(netudendou_50_Obj, transform.position, transform.rotation);
+            netudendou = netudendou_syoki;
             syozi = false;
             respon_OK = false;
         }
@@ -72,10 +88,34 @@ public class PlayerMove : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.Space) && syozi == true && netudendou == 1.0f && respon_OK == true)
         {
             Instantiate(netudendou_100_Obj, transform.position, transform.rotation);
+            netudendou = netudendou_syoki;
             syozi = false;
             respon_OK = false;
         }
     }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "Switch_IceGround" && netudendou == 0)
+        {
+            other.gameObject.GetComponent<IcsSwitch>().Create_IceGround();
+        }
+        if (other.transform.tag == "Switch_IceGround" && netudendou == 1)
+        {
+            other.gameObject.GetComponent<IcsSwitch>().Destory_IceGround();
+        }
+        if (other.transform.tag == "ningen")
+        {
+            if (netudendou == 0) ningenMove.Instance.speed = ningen_idou_osoi;
+            if (netudendou == 0.5) ningenMove.Instance.speed = ningen_idou_nomale;
+            if (netudendou == 1) ningenMove.Instance.speed = ningen_idou_hayai;   
+        }
+
+
+    }
+
+
 
     void OnTriggerStay(Collider other)
     {
@@ -92,6 +132,19 @@ public class PlayerMove : MonoBehaviour {
 
         }
     }
+
+     void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Ground")
+        {
+            isground = true;
+        }
+    
+
+
+    }
+
+
     void OnCollisionStay(Collision collision)
     {
         //今は触れ続けるために移動し続けなければいけない→どうにかしたい
