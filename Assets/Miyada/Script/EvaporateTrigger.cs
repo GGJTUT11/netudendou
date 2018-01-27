8176b9df62bf011cc108d4e5aab1605a397e87b1
宮田　大輝 <hmiyada@miyadahirokinoMacBook-Pro.local>
2018-01-28 01:07:41 +0900

    クマアニメーション管理

--- Assets/Miyada/Script/EvaporateTrigger.cs
+++ Assets/Miyada/Script/EvaporateTrigger.cs
@@ -2,42 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EvaporateTrigger : MonoBehaviour {
+namespace Miyada
+{
+    public class EvaporateTrigger : MonoBehaviour {
 
-    [SerializeField]
-    private EvaporateWater water;
+        [SerializeField]
+        private EvaporateWater water;
 
-    [SerializeField]
-    private GameObject windzone;
+        [SerializeField]
+        private GameObject windzone;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        var player = other.GetComponent<PlayerMove>();
-        if (!player) return;
+        private void OnTriggerEnter(Collider other)
+        {
+            var player = other.GetComponent<PlayerMove>();
+            if (!player) return;
 
-        // 風生成
-        windzone.SetActive(true);
-    }
+            // 風生成
+            windzone.SetActive(true);
+        }
 
-    private void OnTriggerExit(Collider other)
-    {
-        var player = other.GetComponent<PlayerMove>();
-        if (!player) return;
+        private void OnTriggerExit(Collider other)
+        {
+            var player = other.GetComponent<PlayerMove>();
+            if (!player) return;
 
-        // 風消.
-        windzone.SetActive(false);
-    }
+            // 風消.
+            windzone.SetActive(false);
+        }
 
-    private void OnTriggerStay(Collider other)
-    {
-        var player = other.GetComponent<PlayerMove>();
-        if (!player) return;
+        private void OnTriggerStay(Collider other)
+        {
+            var player = other.GetComponent<PlayerMove>();
+            if (!player) return;
 
-        var netu = (int)player.Netudendou_Property;
-        const int HOT = 1;
+            var netu = (int)player.Netudendou_Property;
+            const int HOT = 1;
 
-        if(netu == HOT) {
-            water.Evaporate();
+            if(netu == HOT) {
+                water.Evaporate();
+            }
         }
     }
 }
